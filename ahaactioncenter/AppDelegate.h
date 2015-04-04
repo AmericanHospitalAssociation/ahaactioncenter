d4d5f4c47701ef53582365d8c0116654baae070f
Vince Davis <vince.davis@me.com>
2015-04-04 15:08:12 -0500

    Initial Commit

---
+++ ahaactioncenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ahaactioncenter
+//
+//  Created by Server on 4/4/15.
+//  Copyright (c) 2015 AHA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ahaactioncenter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ahaactioncenter
+//
+//  Created by Server on 4/4/15.
+//  Copyright (c) 2015 AHA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
